1122bb5cae35e256bb004be62ad86a8cf5144696
Alvaro-Cabrera <alvaro.cabrera@bootcamp-upgrade.com>
2023-08-22 15:26:12 +0200

    twitter

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$tweets =[
+    ["autor" => "Alvaro", "texto" =>"Mi primer twitt!!", "usuario" => "CokeTouch"], ["autor" => "Dayana", "texto" =>"Me tocó la loteria por fin!!", "usuario" => "DayanaUpgrade"], ["autor" => "Sara", "texto" =>"Otro twitt!!", "usuario" => "SaraUpgrade"]];
+
+require_once "utils.php";
+require_once "index.html";
+
+?>
